1c09a37ec047d81a2a7947feedb0a1fe41e92736
Michael Heilemann <michael.heilemann@me.com>
2024-09-15 20:04:01 -0400

    Build and commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
+	build: {
+		outDir: 'dist',
+		rollupOptions: {
+			input: 'index.html', // Use index.html as the entry point
+		},
+	},
 })
